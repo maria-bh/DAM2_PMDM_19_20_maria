e8b79d9aff33ce5936314bc938470a933c3f5ebf
Mª Alejandra Tomás Fernández <alejandrtf@gmail.com>
2019-10-12 13:32:05 +0200

    Solución Ejercicio 2 Layouts Sencillos usando LinearLayout añadida

---
+++ ANDROID/SolucionEj2LayoutsVisualSencillos/LinearLayoutSolucion/src/main/java/es/alejandrtf/solucionej2layoutsvisualsencillos/MainActivity.java
@@ -0,0 +1,49 @@
+package es.alejandrtf.solucionej2layoutsvisualsencillos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicial);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.layout_type_here_option_menu:
+                setContentView(R.layout.type_here_layout);
+                return true;
+            case R.id.layout_colores_centrados_option_menu:
+                setContentView(R.layout.colores_centrados_layout);
+                return true;
+            case R.id.layout_colores_escalera_option_menu:
+                setContentView(R.layout.colores_escalera_layout);
+                return true;
+            case R.id.layout_teclado_numeros_option_menu:
+                setContentView(R.layout.teclado_numeros);
+                return true;
+            case R.id.layout_calculadora_option_menu:
+                setContentView(R.layout.calculadora);
+                return true;
+            default:
+                setContentView(R.layout.type_here_layout);
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
