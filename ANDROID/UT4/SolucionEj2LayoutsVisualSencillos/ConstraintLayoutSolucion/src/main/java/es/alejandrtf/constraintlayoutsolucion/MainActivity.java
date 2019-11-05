55ee043be4d91ac0e03439d519b153d3378c09a7
Mª Alejandra Tomás Fernández <alejandrtf@gmail.com>
2019-11-05 16:11:57 +0100

    modificado SolucionEj2LayoutsVisualSencillos para mantener estado al rotar

--- ANDROID/UT4/SolucionEj2LayoutsVisualSencillos/ConstraintLayoutSolucion/src/main/java/es/alejandrtf/constraintlayoutsolucion/MainActivity.java
+++ ANDROID/UT4/SolucionEj2LayoutsVisualSencillos/ConstraintLayoutSolucion/src/main/java/es/alejandrtf/constraintlayoutsolucion/MainActivity.java
@@ -6,11 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private int idLayout;
+    private static String ID_LAYOUT="idLayout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inicial);
+        idLayout=R.layout.inicial;
+
+        if(savedInstanceState!=null)
+        {
+            idLayout=savedInstanceState.getInt(ID_LAYOUT);
+        }
+        setContentView(idLayout);
     }
 
     @Override
@@ -21,25 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.layout_type_here_option_menu:
+                idLayout=R.layout.type_here_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.type_here_layout);
                 return true;
             case R.id.layout_colores_centrados_option_menu:
+                idLayout=R.layout.colores_centrados_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.colores_centrados_layout);
                 return true;
             case R.id.layout_colores_escalera_option_menu:
+                idLayout=R.layout.colores_escalera_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.colores_escalera_layout);
                 return true;
             case R.id.layout_teclado_numeros_option_menu:
+                idLayout=R.layout.teclado_numeros; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.teclado_numeros);
                 return true;
             case R.id.layout_calculadora_option_menu:
+                idLayout=R.layout.calculadora; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.calculadora);
                 return true;
             default:
                 setContentView(R.layout.type_here_layout);
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(ID_LAYOUT,idLayout);
+
     }
 }
--- ANDROID/UT4/SolucionEj2LayoutsVisualSencillos/RelativeLayoutSolucion/src/main/java/es/alejandrtf/relativelayoutsolucion/MainActivity.java
+++ ANDROID/UT4/SolucionEj2LayoutsVisualSencillos/RelativeLayoutSolucion/src/main/java/es/alejandrtf/relativelayoutsolucion/MainActivity.java
@@ -6,12 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int idLayout;
+    private static String ID_LAYOUT = "idLayout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inicial);
+        idLayout = R.layout.inicial;
+
+        if (savedInstanceState != null) {
+            idLayout = savedInstanceState.getInt(ID_LAYOUT);
+        }
+        setContentView(idLayout);
 
     }
 
@@ -26,23 +32,36 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.layout_type_here_option_menu:
+                idLayout = R.layout.type_here_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.type_here_layout);
                 return true;
             case R.id.layout_colores_centrados_option_menu:
+                idLayout = R.layout.colores_centrados_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.colores_centrados_layout);
                 return true;
             case R.id.layout_colores_escalera_option_menu:
+                idLayout = R.layout.colores_escalera_layout; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.colores_escalera_layout);
                 return true;
             case R.id.layout_teclado_numeros_option_menu:
+                idLayout = R.layout.teclado_numeros; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.teclado_numeros);
                 return true;
             case R.id.layout_calculadora_option_menu:
+                idLayout = R.layout.calculadora; //lo guardo para luego restaurar el estado
                 setContentView(R.layout.calculadora);
                 return true;
             default:
                 setContentView(R.layout.type_here_layout);
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(ID_LAYOUT, idLayout);
+
     }
 }
